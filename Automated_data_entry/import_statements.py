52a1aa15324fff1e3e81f340a5bfd4a78e61a3dd
George Kosidis <georgekosidisgk@gmail.com>
2017-11-28 13:03:23 +0200

    Update import_statements.py

--- Automated_data_entry/import_statements.py
+++ Automated_data_entry/import_statements.py
@@ -98,13 +98,13 @@ class Boc_statement:
 
 		def get_our_ref(self):
 
-			Account_names = {"RJ GAMES LTD": "CY01-PUB-BCYP", "RJ DEVELOPMENT LTD": "CY02-BOX-BCYP" }
+			Account_names = {"COMPANY 1 LTD": "CY01-PUB-BCYP", "COMPANY 2": "CY02-BOX-BCYP" }
 
 			return Account_names[str.strip(self.account_name)]
 
 		def get_our_acc_sid(self):
 
-			if int(float(str.strip(self.account_number))) == 357025504963:
+			if int(float(str.strip(self.account_number))) == 00000000000:
 				acc_sid = "CARD"
 			else:
 				acc_sid = "MAIN"
@@ -217,55 +217,3 @@ class Processed_data:
 def writer(destination, file_name, df):
 	os.chdir(destination)
 	df.to_csv(file_name)
-
-
-
-
-# class Data_writer():
-
-# 	def __init__(self, file_path, destination, file_name):
-
-# 		df = Processed_data(file_path)
-
-# 		os.chdir(destination)
-
-# 		df.to_csv(file_name)
-
-
-
-# full_df = Processed_data(boc_folder).processed_dataframe
-
-# Processed_data(boc_folder).writer("C:\\Users\GK\Test Module\data\Destination of BOC CSV", "Log.csv")
-
-
-
-# pro_statements = [Boc_statement(file_name, boc_folder) for file_name in file_names]
-
-
-# print(pro_statements[0].account_number)
-
-# print(pro_statements[0].df_trans.head())
-
-
-# for obj in statements:
-# 	print(obj.dataframe)
-	# for item in obj.description:
-	# 	print(item)
-
-# print(statements[0].transactions[1][7])
-
-# for i in statements:
-# 	print(i.start_date, i.end_date, i.period, i.account_number, i.account_name, i.account_type, i.currency)
-
-# csv_list = []
-
-# for obj in statements:
-# 	with open(obj.file_name) as csvfile:
-# 		csv_data = []
-# 		csv_reader = csv.reader(csvfile)
-# 		for line in csv_reader:
-# 			csv_data.append(line)
-# 		csv_list.append(csv_data)
-
-# for i in csv_list[0]:
-# 	print(i)
